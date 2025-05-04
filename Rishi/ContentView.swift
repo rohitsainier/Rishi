35fe2a71caaa7c3960cb3e3b7150c28f0206259d
Rohit Saini (IND) <rsaini@mafcarrefour.com>
2025-05-04 12:45:13 +0530

    Initial Commit

---
+++ Rishi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rishi
+//
+//  Created by Rohit Saini on 04/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rishi/RishiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RishiApp.swift
+//  Rishi
+//
+//  Created by Rohit Saini on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RishiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
